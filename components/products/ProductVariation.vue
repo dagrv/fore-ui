8abb14603a0ecec9c32289f7c880703134e20070
rust <rust.91@icloud.com>
2021-03-21 17:25:23 +0100

    select product variation to buy

--- components/products/ProductVariation.vue
+++ components/products/ProductVariation.vue
@@ -4,11 +4,10 @@
 
     <div class="control">
       <div class="select is-fullwidth">
-        <select name="" id="">
+        <select :value="selectedVariationId" @change="changed($event, type)">
           <option value="">Choose option</option>
           <option v-for="variation in variations" :key="variation.id" :value="variation.id">
             {{ variation.name }}
-
             <template v-if="variation.price_varies">
               ({{ variation.price }})
             </template>
@@ -29,6 +28,36 @@
       variations: {
         required: true,
         type: Array
+      },
+      value: {
+        required: false,
+        default: ''
+      }
+    },
+
+    computed: {
+      selectedVariationId() {
+        if (!this.findVariation(this.value.id)) {
+          return ''
+        }
+
+        return this.value.id
+      }
+    },
+
+    methods: {
+      changed(event, type) {
+        this.$emit('input', this.findVariation(event.target.value))
+      },
+
+      findVariation(id) {
+        let variation = this.variations.find(v => v.id == id)
+
+        if (typeof variation === 'undefined') {
+          return null
+        }
+
+        return variation
       }
     }
   };
--- pages/products/_slug.vue
+++ pages/products/_slug.vue
@@ -21,7 +21,24 @@
                           :type="type"
                           :variations="variations"
                           :key="type"
+                          v-model="form.variation"
                         />
+
+                        <hr>
+
+                        <div class="field has-addons" v-if="form.variation">
+                          <div class="control">
+                            <div class="select is-fullwidth is-medium">
+                              <select name="" id="">
+                                <option value="">1</option>
+                              </select>
+                            </div>
+                          </div>
+
+                          <div class="control">
+                            <button type="submit" class="button is-success is-rounded is-medium">Add to Cart</button>
+                          </div>
+                        </div>
                       </form>
                     </section>
                 </div>
@@ -36,7 +53,11 @@
 	export default {
     data () {
       return {
-        product: null
+        product: null,
+        form: {
+          variation: '',
+          quantity: 1
+        }
       }
     },
 
