6d373fda78176873cd863876bf9a2563af041f98
rust <rust.91@icloud.com>
2021-03-27 11:48:17 +0100

    disable cart buy button

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -5,11 +5,13 @@
                 <div class="column is-three-quarters">
                     <h1 class="title is-4">Cart</h1>
 
-                    <article class="message">
+                    <article class="message" v-if="products.length">
                         <CartOverview />
                     </article>
 
-                    <a href="#" class="button is-fullwidth is-medium is-info is-success is-size-5 has-text-weight-semibold">
+                    <h1 v-else class="is-size-3 has-text-centered mt-6">Your cart is empty</h1>
+
+                    <a href="#" v-if="!empty" class="button is-fullwidth is-success is-size-5 has-text-weight-semibold">
                         Buy
                     </a>
                 </div>
@@ -19,11 +21,19 @@
 </template>
 
 <script>
+    import { mapGetters } from 'vuex'
     import CartOverview from '@/components/cart/CartOverview'
     
     export default {
         components: {
             CartOverview
+        },
+
+        computed: {
+            ...mapGetters({
+                empty: 'cart/empty',
+                products: 'cart/products',
+            })
         }
     }
 </script>
