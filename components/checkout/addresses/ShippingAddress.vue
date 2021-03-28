dbf0151ec1c66ed0bd18aba4f7390e631493a0ff
rust <rust.91@icloud.com>
2021-03-28 17:36:35 +0200

    change default shipping address

--- components/checkout/addresses/ShippingAddress.vue
+++ components/checkout/addresses/ShippingAddress.vue
@@ -2,24 +2,52 @@
     <article class="message is-dark">
         <div class="message-body">
         <h1 class="title is-5">Ship to</h1>
-            <template v-if="selectedAddress">
-                {{ selectedAddress.name }} <br>
-                {{ selectedAddress.address_1 }} <br>
-                {{ selectedAddress.city }} <br>
-                {{ selectedAddress.postal_code }}<br>
-                {{ selectedAddress.country.name }} <br>
+            
+            <template v-if="selecting">
+                <ShippingAddressSelector
+                    :addresses="addresses"
+                    :selectedAddress="selectedAddress"
+                    @click="addressSelected"
+                />
+            </template>
+
+            <template v-else>
+                <template v-if="selectedAddress">
+                    <p>
+                        {{ selectedAddress.name }} <br>
+                        {{ selectedAddress.address_1 }} <br>
+                        {{ selectedAddress.city }} <br>
+                        {{ selectedAddress.postal_code }}<br>
+                        {{ selectedAddress.country.name }} <br>
+                    </p>
+                </template>
+                <br>
+                <div class="field is-grouped">
+                    <p class="control">
+                        <a href="" class="button is-link is-outlined is-size-6" @click.prevent="selecting = true">
+                            Change Shipping Address
+                        </a>
+                    </p>
+                </div>
             </template>
         </div>
     </article>
 </template>
 
 <script>
+    import ShippingAddressSelector from './ShippingAddressSelector'
+
     export default {
         data() {
             return {
+                selecting: false,
                 localAddresses: this.addresses,
                 selectedAddress: null
             }
+        },
+
+        components: {
+            ShippingAddressSelector
         },
         
         props: {
@@ -36,6 +64,11 @@
         },
 
         methods: {
+            addressSelected(address) {
+                this.switchAddress(address)
+                this.selecting = false
+            },
+
             switchAddress(address) {
                 this.selectedAddress = address
             }
